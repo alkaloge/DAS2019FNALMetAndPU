e210cb11371a646802b62a1e02542441e6aabd06
Alexis Kalogeropoulos <alkaloge@cern.ch>
2020-06-28 09:03:41 -0500

    first commit

--- MetAndPu/MetAndPuAnal/test/run_MetAndPuAnalStep2A_cfg.py
+++ MetAndPu/MetAndPuAnal/test/run_MetAndPuAnalStep2A_cfg.py
@@ -8,19 +8,17 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string("./outputs/metandpu_step2.root") )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
 
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring([
-            #'file:/eos/uscms/store/user/cmsdas/2019/short_exercises/METAndPU/data/handson1_dy1jet_m50_fall17_part.root',
-            #'file:/eos/uscms/store/user/cmsdas/2019/short_exercises/METAndPU/data/handson1_dylljets_amcatnlo.root',
-            '/store/user/cmsdas/2019/short_exercises/METAndPU/data/handson1_dylljets_amcatnlo.root',
+            '/store/user/cmsdas/2020/short_exercises/METAndPU/MC/handson1_dy1jet_madgraph_m50_autumn18_part.root',
             ]
                                       )
     )
